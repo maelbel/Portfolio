7d269aee74b1b7a770452f9d90ab3c8d9c254b93
m b <mael.belliard@laposte.net>
2024-09-16 16:29:01 +0200

    fixed theme

--- app/ui/theme-toggle.tsx
+++ app/ui/theme-toggle.tsx
@@ -14,10 +14,11 @@ const ThemeToggle = () => {
 	return (
 		<button 
             title='Basculer entre le thème sombre et clair'
-            className="flex justify-center items-center h-[44px] rounded-full p-3 text-xs font-medium hover:bg-gray-100 dark:hover:bg-gray-800 md:flex-none md:justify-start md:p-2 md:px-3 transition-all duration-400" 
+            className="relative flex justify-center items-center h-[44px] rounded-full p-3 text-xs font-medium hover:bg-gray-100 dark:hover:bg-gray-800 md:flex-none md:justify-start md:p-2 md:px-3 transition-all duration-400" 
             onClick={toggleTheme}>
             	<SunIcon className='block dark:hidden w-5' />
             	<MoonIcon className='hidden dark:block w-5'/>
+				<span className='sr-only'>{theme}</span>
         </button>
 	);
 };
